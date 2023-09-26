512fa7c0405b7157aff29bd8a81dfadb36742bd4
kuipid01 <kuipid01@gmail.com>
2023-09-26 17:37:12 +0100

    first commit

--- src/components/Pageloader/Loader.jsx
+++ src/components/Pageloader/Loader.jsx
@@ -7,7 +7,10 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useEffect } from "react";
 const Loader = () => {
   const [textIndex, setTextIndex] = useState(0);
-  const [dimension, setDimension] = useState({width:0,height:0});
+  const [width, setwidth] = useState(window.innerWidth);
+  // const [loding, setloding] = useState(false);
+  const [pageLoaing, setpageLoaing] = useState(true);
+  const [height, setHeight] = useState();
   const textArray = ["welcome", "kuipid", "messi", "peter obi"];
   useEffect(() => {
     const intervalId = setInterval(
@@ -20,9 +23,23 @@ const Loader = () => {
     // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
   }, []);
-useEffect(() => {
-  setDimension({width:window.innerWidth, height:window.innerHeight})
-}, [])
+
+  useEffect(() => {
+    const hanleResize = () => {
+      setwidth(window.innerWidth);
+    };
+    window.addEventListener("resize", hanleResize);
+    return () => {
+      window.removeEventListener("resize", hanleResize);
+    };
+  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setpageLoaing(false);
+      // document.body.style.cursor = "default";
+      window.scrollTo(0, 0);
+    }, 2000);
+  }, []);
   const textVariant = {
     initial: {
       opacity: 1,
@@ -35,42 +52,43 @@ useEffect(() => {
       opacity: 0,
     },
   };
-  const slideUp = {
+  const initialPath = `M0,0  Q${width / 2},${width / 3.5} ${width}, 0 Z `;
+  const targetPath = `M-${width * 3},0  Q${width * 0},0 ${width * 3}, 0 Z`;
+
+  const slieUp = {
     initial: {
       y: "0%",
+      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
     },
 
     exit: {
       y: "-100%",
-      transition: { duration: .8, ease: [0.76, 0, 0.24, 1], delay: 0.2 },
+      transition: { duration: 1.5, ease: [0.76, 0, 0.24, 1] },
     },
   };
-  const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2}  ${dimension.height * 300} 0 ${dimension.height} L0 0 `; // Flat initial path
-  const targetPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${dimension.height} Q${dimension.width/2}   ${dimension.height } 0 ${dimension.height} L0 0 `; // Flat initial path
 
-
-  
-  const curve = {
-    initial: {
+  const LoaerVariant = {
+    enter: {
       d: initialPath,
+      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
     },
-
 
     exit: {
       d: targetPath,
-
-      transition: { duration: .8, ease: [0.76, 0, 0.24, 1] },
+      y: "-20%",
+      transition: { duration: 2.5, ease: [0.76, 0, 0.24, 1] },
     },
   };
   //   console.log(textIndex);
   return (
     <>
       <motion.div
-        variants={slideUp}
+        variants={slieUp}
         initial="initial"
         animate="enter"
         exit="exit"
-        className="loader"
+        key={1}
+        className={`loading`}
       >
         <motion.div
           variants={textVariant}
@@ -82,22 +100,17 @@ useEffect(() => {
         >
           {textArray[textIndex]}
         </motion.div>
+        <svg width="100%" height="100vh">
+              <motion.path
+                variants={LoaerVariant}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+                fill="#886D72"
+              />
+            </svg>
       </motion.div>
-      {/* <svg className="svgLoader">
-        <motion.path
-        //   variants={curve}
-        //   initial="initial"
-        //   animate="enter"
-        //   exit="exit"
-        //    fill="blue"
-  stroke="blue"
-  stroke-width="2"
-          d={targetPath}
-        ></motion.path>
-      </svg> */}
-     <motion.svg className='svgLoader' >
-  <motion.path variants={curve} initial='initial' exit='exit' d={initialPath} fill='blue' />
-</motion.svg>
+   
 
     </>
   );
